35746f61896bcd8dcbefd88a55ca3ade774ab06e
castelojb <joaocb14@gmail.com>
2022-04-30 14:28:19 -0300

    Adding job 2 specs

---
+++ ml_core/_abstract_models.py
@@ -0,0 +1,29 @@
+from abc import abstractmethod
+
+import numpy as np
+
+class MlModel:
+
+	@abstractmethod
+	def first_model(self, **kwargs) -> 'MlModel': pass
+
+	@abstractmethod
+	def predict(self, x: np.ndarray) -> np.ndarray: pass
+
+	@abstractmethod
+	def update(self, **kwargs): pass
+
+	@abstractmethod
+	def is_close(self, model: 'MlModel', **kwargs) -> bool: pass
+
+	@abstractmethod
+	def __copy__(self): pass
+
+	@abstractmethod
+	def __str__(self): pass
+
+
+class MlAlgoritm:
+
+	@abstractmethod
+	def fit(self, x: np.ndarray, y: np.ndarray, **kwargs) -> MlModel | list[MlModel]: pass
